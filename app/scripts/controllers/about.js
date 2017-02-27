2539484ae8b5103109077c1f0b6e92b85e78f917
Milos Zivadinovic <Milos Zivadinovic>
2017-02-27 21:52:48 +0100

    First commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name aspirineApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the aspirineApp
+ */
+angular.module('aspirineApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name aspirineApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the aspirineApp
+ */
+angular.module('aspirineApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
